88e702a8cfa495e0439e438df674272aa697ca30
jvanalytics <joao.valente.work@gmail.com>
2024-07-29 22:21:39 +0100

    simplified app running

--- app.py
+++ app.py
@@ -85,6 +85,8 @@ filepath = 'data_set_da_test.csv'
 
 df = pd.read_csv(filepath)
 
+sql_df = df  # use for sql exercises
+
 total_sessions = df[scope].nunique()
 
 
@@ -189,120 +191,114 @@ with tab1:
 
     # Funnel Analysis per Page Type
 
-    with st.spinner(text="Loading"):
+    st.subheader("Insights")
 
-        st.subheader("Insights")
+    if page_type == None:
+        st.markdown(
+            r"""
+        Almost 100% of result in some but only 3% result in add to cart. However it has a very high overall cart conversion rate of 71.6% which indicates good product and shopping experience after adding to cart.
 
-        if page_type == None:
-            st.markdown(
-                r"""
-            Almost 100% of result in some but only 3% result in add to cart. However it has a very high overall cart conversion rate of 71.6% which indicates good product and shopping experience after adding to cart.
-
-        """
-            )
-        elif page_type == "Search Listing Page":
-            st.markdown(
-                r"""
-            Only 4% of sessions result in a user making first a session. Also, only 6.8% of those add a product to cart which indicates search might need to be improved or experiment giving it higher visibility when a user accesses the website/app.
-        """
-            )
-
-        elif page_type == "Listing Page":
-            st.markdown(
-                r"""
-            Users access first the Listing Page in 52% of sessions which should indicate that menu and category structures and visibility should be good.
-            However, considering that users can add products through the listing page,less that 1% actually do it.
-            Most likely, users will prefer to go to the Product Page to know more about the products before adding it. Curious that this does not happen the same way in Search Listing Page.
-        """
-            )
-
-        elif page_type == "Product Page":
-            st.markdown(
-                r"""
-            Users access Product Page 48% of sessions, which is also a good indicator of its easy discovery.
-            It is also the main way of adding products to the cart (3%) and then purchase (1%).
-        """
-            )
-
-        st.divider()
-
-        col1, col2, col3 = st.columns(3)
-
-        with col1:
-            st.metric(f"{scope.capitalize()}s (Overall | Interested)",
-                      interested_sessions)
-            st.metric(f"Purchase {scope.capitalize()}s", purchase_sessions)
-
-        with col2:
-            st.metric("Add to Cart Rate", f'{add_to_cart_rate} %')
-            st.metric("Conversion Rate (Overall | Interested)",
-                      f'{session_cr} %')
-
-        with col3:
-            st.metric(f"Add to Cart {scope.capitalize()}s",
-                      add_to_cart_sessions)
-            st.metric("Cart Abandonment Rate", f'{cart_abandonment_rate} %')
-
-        st.divider()
-
-        st.subheader('Funnel Visualization by First Type page visited')
-
-        # Plotly funnel chart
-        stages = ['Total Sessions', 'Interested Sessions',
-                  'Add to Cart Sessions', 'Purchase Sessions']
-        values = [metrics['sessions'], metrics['interested_sessions'],
-                  metrics['add_to_cart_sessions'], metrics['purchase_sessions']]
-
-        fig = go.Figure(go.Funnel(
-            y=stages,
-            x=values,
-            textinfo="value+percent initial"
-        ))
-
-        fig.update_layout(title="Funnel Visualization")
-
-        st.plotly_chart(fig)
-
-        st.divider()
-
-        st.subheader("Top 50 Products Added to Cart")
-        # Calculate the number of unique products added to the cart per product
-
-        df_product_overall_top_atc = df_first[df_first['event_type'] == 'add_to_cart'].groupby(
-            'product')[scope].nunique().reset_index(name=f'Add to Cart {scope.capitalize()}s')
-
-        df_product_pageview_top_atc = df_first[(df_first['event_type'] == 'add_to_cart') & (df_first['page_type_first'] == 'product_page')].groupby(
-            'product')[scope].nunique().reset_index(name=f'Product Page Add to Cart {scope.capitalize()}s')
-
-        df_product_listing_top_atc = df_first[(df_first['event_type'] == 'add_to_cart') & (df_first['page_type_first'] == 'listing_page')].groupby(
-            'product')[scope].nunique().reset_index(name=f'Listing Page Add to Cart {scope.capitalize()}s')
-
-        df_product_search_listing_atc = df_first[(df_first['event_type'] == 'add_to_cart') & (df_first['page_type_first'] == 'search_listing_page')].groupby(
-            'product')[scope].nunique().reset_index(name=f'Search Listing Page Add to Cart {scope.capitalize()}s')
-
-        merged_product_df = df_product_overall_top_atc.merge(
-            df_product_pageview_top_atc, on='product', how='outer'
-
-        ).merge(
-            df_product_listing_top_atc, on='product', how='outer'
-
-        ).merge(
-            df_product_search_listing_atc, on='product', how='outer'
+    """
+        )
+    elif page_type == "Search Listing Page":
+        st.markdown(
+            r"""
+        Only 4% of sessions result in a user making first a session. Also, only 6.8% of those add a product to cart which indicates search might need to be improved or experiment giving it higher visibility when a user accesses the website/app.
+    """
         )
 
-        df['product'] = df['product'].astype('string')
+    elif page_type == "Listing Page":
+        st.markdown(
+            r"""
+        Users access first the Listing Page in 52% of sessions which should indicate that menu and category structures and visibility should be good.
+        However, considering that users can add products through the listing page,less that 1% actually do it.
+        Most likely, users will prefer to go to the Product Page to know more about the products before adding it. Curious that this does not happen the same way in Search Listing Page.
+    """
+        )
 
-        top_50_products = merged_product_df.sort_values(
-            by=merged_product_df.columns[1], ascending=False).head(50)
+    elif page_type == "Product Page":
+        st.markdown(
+            r"""
+        Users access Product Page 48% of sessions, which is also a good indicator of its easy discovery.
+        It is also the main way of adding products to the cart (3%) and then purchase (1%).
+    """
+        )
 
-        st.dataframe(top_50_products)
+    st.divider()
 
-    st.success("Done!")
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.metric(f"{scope.capitalize()}s (Overall | Interested)",
+                  interested_sessions)
+        st.metric(f"Purchase {scope.capitalize()}s", purchase_sessions)
+
+    with col2:
+        st.metric("Add to Cart Rate", f'{add_to_cart_rate} %')
+        st.metric("Conversion Rate (Overall | Interested)",
+                  f'{session_cr} %')
+
+    with col3:
+        st.metric(f"Add to Cart {scope.capitalize()}s",
+                  add_to_cart_sessions)
+        st.metric("Cart Abandonment Rate", f'{cart_abandonment_rate} %')
+
+    st.divider()
+
+    st.subheader('Funnel Visualization by First Type page visited')
+
+    # Plotly funnel chart
+    stages = ['Total Sessions', 'Interested Sessions',
+              'Add to Cart Sessions', 'Purchase Sessions']
+    values = [metrics['sessions'], metrics['interested_sessions'],
+              metrics['add_to_cart_sessions'], metrics['purchase_sessions']]
+
+    fig = go.Figure(go.Funnel(
+        y=stages,
+        x=values,
+        textinfo="value+percent initial"
+    ))
+
+    fig.update_layout(title="Funnel Visualization")
+
+    st.plotly_chart(fig)
+
+    st.divider()
+
+    st.subheader("Top 50 Products Added to Cart")
+    # Calculate the number of unique products added to the cart per product
+
+    df_product_overall_top_atc = df_first[df_first['event_type'] == 'add_to_cart'].groupby(
+        'product')[scope].nunique().reset_index(name=f'Add to Cart {scope.capitalize()}s')
+
+    df_product_pageview_top_atc = df_first[(df_first['event_type'] == 'add_to_cart') & (df_first['page_type_first'] == 'product_page')].groupby(
+        'product')[scope].nunique().reset_index(name=f'Product Page Add to Cart {scope.capitalize()}s')
+
+    df_product_listing_top_atc = df_first[(df_first['event_type'] == 'add_to_cart') & (df_first['page_type_first'] == 'listing_page')].groupby(
+        'product')[scope].nunique().reset_index(name=f'Listing Page Add to Cart {scope.capitalize()}s')
+
+    df_product_search_listing_atc = df_first[(df_first['event_type'] == 'add_to_cart') & (df_first['page_type_first'] == 'search_listing_page')].groupby(
+        'product')[scope].nunique().reset_index(name=f'Search Listing Page Add to Cart {scope.capitalize()}s')
+
+    merged_product_df = df_product_overall_top_atc.merge(
+        df_product_pageview_top_atc, on='product', how='outer'
+
+    ).merge(
+        df_product_listing_top_atc, on='product', how='outer'
+
+    ).merge(
+        df_product_search_listing_atc, on='product', how='outer'
+    )
+
+    df['product'] = df['product'].astype('string')
+
+    top_50_products = merged_product_df.sort_values(
+        by=merged_product_df.columns[1], ascending=False).head(50)
+
+    st.dataframe(top_50_products)
+
 
 # second part of exercise (SQL Queries)
-
-
-sql_df = pd.read_csv(filepath)
 
 # ensure datetime format
 sql_df['event_date'] = pd.to_datetime(sql_df['event_date'])
@@ -351,79 +347,72 @@ GROUP BY date;
 first_query_result = ps.sqldf(query, locals())
 
 with tab2:
-    with st.spinner(text="Loading"):
+    st.subheader(
+        "Query that displays number of users per day that only viewed products in their first session")
+    st.code(query, language="sql")
 
-        st.subheader(
-            "Query that displays number of users per day that only viewed products in their first session")
-        st.code(query, language="sql")
-
-        st.subheader("1st query output")
-        st.dataframe(first_query_result)
-
-    st.success("Done!")
+    st.subheader("1st query output")
+    st.dataframe(first_query_result)
 
 
 # second sql query
 
 
 with tab3:
-    with st.spinner(text="Loading"):
-        st.subheader(
-            "Query that will return any abnormal user behavior")
+    st.subheader(
+        "Query that will return any abnormal user behavior")
 
-        st.markdown(
-            """Here we are trying to analyze traffic outliers (abnormal session counts). 
-            Ideally I would prefer to consider any traffic count that goes beyond a certain percentile (like 75th or 95th percentile).
-            However the pandasql library uses SQLite and we cannot perform percentile calculation with it. 
-            So in that case I made it a bit more interactive by calculating the average session count per user and calculate the abnormal values based on a user input multiplier that you can change.
-            This query can also be adapted to detect abnormal event actions like add to cart or purchase or even session duration.
-            """""
-        )
+    st.markdown(
+        """Here we are trying to analyze traffic outliers (abnormal session counts). 
+        Ideally I would prefer to consider any traffic count that goes beyond a certain percentile (like 75th or 95th percentile).
+        However the pandasql library uses SQLite and we cannot perform percentile calculation with it. 
+        So in that case I made it a bit more interactive by calculating the average session count per user and calculate the abnormal values based on a user input multiplier that you can change.
+        This query can also be adapted to detect abnormal event actions like add to cart or purchase or even session duration.
+        """""
+    )
 
-        multiplier = st.slider("Select Multiplier to detect abnormal Session Count per User",
-                               min_value=1.0, max_value=8.0, value=3.0, step=0.5)
+    multiplier = st.slider("Select Multiplier to detect abnormal Session Count per User",
+                           min_value=1.0, max_value=8.0, value=3.0, step=0.5)
 
-        second_query = f"""
-        WITH user_summary AS (
-        SELECT
-            user,
-            COUNT(*) AS total_sessions,
-            COUNT(DISTINCT page_type) AS page_types_visited
-        FROM sql_df
-        GROUP BY user
-        ),
-        avg_values AS (
-        SELECT
-            AVG(total_sessions) AS avg_total_sessions
-        FROM user_summary
-        )
-
-        SELECT
+    second_query = f"""
+    WITH user_summary AS (
+    SELECT
         user,
-        total_sessions,
-        page_types_visited,
-        CASE
-            WHEN total_sessions > {multiplier} * avg_values.avg_total_sessions THEN 'Abnormal Session Count'
-            ELSE 'Normal'
-        END AS abnormal_behavior
-        FROM user_summary, avg_values
-        """
+        COUNT(*) AS total_sessions,
+        COUNT(DISTINCT page_type) AS page_types_visited
+    FROM sql_df
+    GROUP BY user
+    ),
+    avg_values AS (
+    SELECT
+        AVG(total_sessions) AS avg_total_sessions
+    FROM user_summary
+    )
 
-        # Run the SQL query on the DataFrame
-        second_query_result = ps.sqldf(second_query, locals())
+    SELECT
+    user,
+    total_sessions,
+    page_types_visited,
+    CASE
+        WHEN total_sessions > {multiplier} * avg_values.avg_total_sessions THEN 'Abnormal Session Count'
+        ELSE 'Normal'
+    END AS abnormal_behavior
+    FROM user_summary, avg_values
+    """
 
-        st.code(second_query, language="sql")
+    # Run the SQL query on the DataFrame
+    second_query_result = ps.sqldf(second_query, locals())
 
-        st.subheader("2nd query output")
+    st.code(second_query, language="sql")
 
-        st.button("Reset", type="primary")
+    st.subheader("2nd query output")
 
-        if st.button("Toggle Abnormal Only"):
-            second_query_df = second_query_result[second_query_result['abnormal_behavior']
-                                                  == 'Abnormal Session Count']
-        else:
-            second_query_df = second_query_result
+    st.button("Reset", type="primary")
 
-        st.dataframe(second_query_df)
+    if st.button("Toggle Abnormal Only"):
+        second_query_df = second_query_result[second_query_result['abnormal_behavior']
+                                              == 'Abnormal Session Count']
+    else:
+        second_query_df = second_query_result
 
-    st.success("Done!")
+    st.dataframe(second_query_df)
